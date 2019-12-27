c4cff98c75b97e8804d83a25bd085817af2b1283
Manuel Kamper <manuel@kmpr.at>
2019-12-27 20:00:22 +0100

    Projektdateien hinzufügen.

---
+++ Mk0.Software.MD5Generator/Main.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Mk0.Software.MD5Generator
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.buttonSelectFile = new System.Windows.Forms.Button();
+            this.textBoxMD5 = new System.Windows.Forms.TextBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // buttonSelectFile
+            // 
+            this.buttonSelectFile.Location = new System.Drawing.Point(12, 12);
+            this.buttonSelectFile.Name = "buttonSelectFile";
+            this.buttonSelectFile.Size = new System.Drawing.Size(142, 23);
+            this.buttonSelectFile.TabIndex = 1;
+            this.buttonSelectFile.Text = "Datei wählen...";
+            this.buttonSelectFile.UseVisualStyleBackColor = true;
+            this.buttonSelectFile.Click += new System.EventHandler(this.ButtonSelectFile_Click);
+            // 
+            // textBoxMD5
+            // 
+            this.textBoxMD5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxMD5.BackColor = System.Drawing.SystemColors.Window;
+            this.textBoxMD5.Location = new System.Drawing.Point(12, 41);
+            this.textBoxMD5.Multiline = true;
+            this.textBoxMD5.Name = "textBoxMD5";
+            this.textBoxMD5.ReadOnly = true;
+            this.textBoxMD5.Size = new System.Drawing.Size(360, 108);
+            this.textBoxMD5.TabIndex = 2;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 161);
+            this.Controls.Add(this.textBoxMD5);
+            this.Controls.Add(this.buttonSelectFile);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(400, 200);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(400, 200);
+            this.Name = "Main";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MD5 Generator v1.0 | © 2019-2020 by mk0.at";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSelectFile;
+        private System.Windows.Forms.TextBox textBoxMD5;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+    }
+}
+
---
+++ Mk0.Software.MD5Generator/Main.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mk0.Software.MD5Generator
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonSelectFile_Click(object sender, EventArgs e)
+        {
+            DialogResult res = openFileDialog.ShowDialog();
+            if(res == DialogResult.OK)
+            {
+                GenerateMD5(openFileDialog.FileName);
+            }
+        }
+
+        private void GenerateMD5(string filename)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(filename))
+                {
+                    textBoxMD5.Text = BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", string.Empty);
+                }
+            }
+        }
+    }
+}
